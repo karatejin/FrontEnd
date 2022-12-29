d1b9b403ead94d5d2c2c2ecda1ae4757ea0d9c52
karatejin <junsyang@naver.com>
2022-12-29 12:46:38 +0900

    20221229 자바스크립트

---
+++ javascript/js/commom.js
@@ -0,0 +1,11 @@
+const helloworld = () => {
+    alert('HelloWorld');
+}
+
+const moveExample02 = () => { 
+    location.replace('./example02.html');    
+}
+
+const back = () => {
+    history.back();
+}
